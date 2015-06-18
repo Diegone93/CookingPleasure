4b1110023106930ae74de133444988cf33fb03c9
Giammarco Agazzotti <giammarcoagazzotti@gmail.com>
2015-06-18 16:08:27 +0200

    Added website to the project

--- CookingPleasure/settings.py
+++ CookingPleasure/settings.py
@@ -1,3 +1,13 @@
 ALLOWED_HOSTS = ['www.example.com']
 DEBUG = True
 DEFAULT_FROM_EMAIL = 'webmaster@example.com'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CookingPleasure',
+        'USER': 'developerAccess',
+        'PASSWORD': 'developeraccess',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
