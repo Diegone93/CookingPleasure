ff6192a84775a2f9d05063db0af83ee40729ffc4
Giammarco Agazzotti <giammarcoagazzotti@gmail.com>
2015-06-19 11:19:06 +0200

    Models updated

--- CookingPleasure/models.py
+++ CookingPleasure/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django import forms
 
 class User(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     nickname = models.CharField(max_length=100, primary_key=True)
-    mail = models.CharField(max_length=100)
+    mail = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=50)
 
     def __unicode__(self):
@@ -13,33 +12,48 @@ class User(models.Model):
 
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=100, primary_key=True)
+    auto_increment_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
     content = models.TextField()
     author = models.CharField(max_length=100)
-    menu = models.ForeignKey(Menu)
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User.nickname)
 
     def __unicode__(self):
         return u'%s' % (self.name)
 
+# Switch tables BEGIN
+class RecipeMenuContains(models.Model):
+    menu_id = models.ForeignKey(Menu.auto_increment_id)
+    recipe_id = models.ForeignKey(Recipe.auto_increment_id)
+
+
+class RecipeIngredientContains(models.Model):
+    recipe_id = models.ForeignKey(Recipe.auto_increment_id)
+    ingredient_name = models.ForeignKey(Ingredient.name)
+
+class ListIngredientContains(models.Model):
+    ingredient_name = models.ForeignKey(Ingredient.name)
+    list_id = models.ForeignKey(List.auto_increment_id)
+# Switch tables END
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=50, primary_key=True)
-    recipe = models.ForeignKey(Recipe)
-    list = models.ForeignKey(List)
 
     def __unicode__(self):
         return u'%s' % (self.name)
 
 class Menu(models.Model):
-    name = models.CharField(max_length=100, primary_key=True)
-    user = models.ForeignKey(User)
+    auto_increment_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User.nickname)
 
     def __unicode__(self):
         return u'%s' % (self.name)
 
 class List(models.Model):
-    name = models.CharField(max_length=100, primary_key=True)
-    user = models.ForeignKey(User)
+    auto_increment_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User.nickname)
 
     def __unicode__(self):
         return u'%s' % (self.name)
