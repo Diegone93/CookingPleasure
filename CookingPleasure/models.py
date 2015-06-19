b43f498a5b855fbe24d8c40e852ad3313898efee
Giammarco Agazzotti <giammarcoagazzotti@gmail.com>
2015-06-19 10:53:36 +0200

    Added models

--- CookingPleasure/models.py
+++ CookingPleasure/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 from django import forms
 
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    nickname = models.CharField(max_length=100, primary_key=True)
+    mail = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return u'%s' % (self.nickname)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100, primary_key=True)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    menu = models.ForeignKey(Menu)
+    user = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return u'%s' % (self.name)
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50, primary_key=True)
+    recipe = models.ForeignKey(Recipe)
+    list = models.ForeignKey(List)
+
+    def __unicode__(self):
+        return u'%s' % (self.name)
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100, primary_key=True)
+    user = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return u'%s' % (self.name)
+
+class List(models.Model):
+    name = models.CharField(max_length=100, primary_key=True)
+    user = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return u'%s' % (self.name)
--- CookingPleasure/settings.py
+++ CookingPleasure/settings.py
@@ -1,3 +1,5 @@
+import os.path
+
 ALLOWED_HOSTS = ['www.example.com']
 DEBUG = True
 DEFAULT_FROM_EMAIL = 'webmaster@example.com'
@@ -11,3 +13,13 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.admin'
+)
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'website/templates')
+)
